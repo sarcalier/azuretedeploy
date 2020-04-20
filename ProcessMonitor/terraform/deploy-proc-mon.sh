b34a5013dd2a85e026e9a033981e3bec793c4b2c
Ruslan Gatiyatullin <Ruslan.Gatiyatullin@icl-services.com>
2020-04-20 11:18:23 +0300

    switching to master branch

--- ProcessMonitor/terraform/deploy-proc-mon.sh
+++ ProcessMonitor/terraform/deploy-proc-mon.sh
@@ -4,7 +4,7 @@ echo "Creating the directory"
 mkdir tf-deploy-proc-mon
 cd tf-deploy-proc-mon
 echo "Downloading the terraform manifest"
-wget "https://raw.githubusercontent.com/sarcalier/azuretedeploy/dev/ProcessMonitor/terraform/main.tf"
+wget "https://raw.githubusercontent.com/sarcalier/azuretedeploy/master/ProcessMonitor/terraform/main.tf"
 echo "Running the terraform manifest"
 terraform init
 terraform apply
