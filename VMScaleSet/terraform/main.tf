c889da7de729505072a219fbcfdadc27ba77d035
Ruslan Gatiyatullin <Ruslan.Gatiyatullin@icl-services.com>
2020-05-05 13:14:33 +0300

    fixing subnet prefix warning

--- VMScaleSet/terraform/main.tf
+++ VMScaleSet/terraform/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_subnet" "vmss" {
   name                 = "vmss-subnet"
   resource_group_name  = azurerm_resource_group.vmss.name
   virtual_network_name = azurerm_virtual_network.vmss.name
-  address_prefix       = "10.0.2.0/24"
+  address_prefixes       = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "vmss" {
