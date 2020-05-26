5746bd5c8b28ba2c7b7103bef7fbc222636b786b
Ruslan Gatiyatullin <Ruslan.Gatiyatullin@icl-services.com>
2020-05-26 16:13:17 +0300

    2 scale set and an NSG

--- VMSSPatching/terraform/st02/main.tf
+++ VMSSPatching/terraform/st02/main.tf
@@ -48,6 +48,14 @@ data "azurerm_shared_image_version" "first" {
   resource_group_name = var.imgRGname
 }
 
+data "azurerm_shared_image_version" "second" {
+  name                = "1.0.1"
+  image_name          = "ubuntu_nginx"
+  gallery_name        = "vmss_imgal"
+  resource_group_name = var.imgRGname
+}
+
+
 
 resource "azurerm_public_ip" "vmss" {
   name                         = "vmss-public-ip"
@@ -55,7 +63,7 @@ resource "azurerm_public_ip" "vmss" {
   resource_group_name          = azurerm_resource_group.vmsspatchrg.name
   allocation_method            = "Static"
   domain_name_label            = "${lower(var.prefix)}-${random_string.token1.result}"
-  #sku                          = "Standard"
+  sku                          = "Standard"
 }
 
 
@@ -75,11 +83,18 @@ resource "azurerm_subnet" "vmssbpatch" {
   address_prefixes       = ["10.0.4.0/24"]
 }
 
+resource "azurerm_subnet" "vmssbpatch2" {
+  name                 = "vmss-subnet2"
+  resource_group_name  = azurerm_resource_group.vmsspatchrg.name
+  virtual_network_name = azurerm_virtual_network.vmss.name
+  address_prefixes       = ["10.0.5.0/24"]
+}
+
 resource "azurerm_lb" "vmssbpatch" {
   name                = "vmss-lb"
   location            = azurerm_resource_group.vmsspatchrg.location
   resource_group_name = azurerm_resource_group.vmsspatchrg.name
-  #sku                 = "Standard"
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
@@ -92,6 +107,12 @@ resource "azurerm_lb_backend_address_pool" "bpepool" {
   resource_group_name = azurerm_resource_group.vmsspatchrg.name
   loadbalancer_id     = azurerm_lb.vmssbpatch.id
   name                = "BackEndAddressPool01"
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool2" {
+  resource_group_name = azurerm_resource_group.vmsspatchrg.name
+  loadbalancer_id     = azurerm_lb.vmssbpatch.id
+  name                = "BackEndAddressPool02"
 }
 
 resource "azurerm_lb_probe" "vmsspatch" {
@@ -126,6 +147,48 @@ resource "azurerm_lb_rule" "lbnatrulessh" {
   #probe_id                       = azurerm_lb_probe.vmsspatch.id
 }
 
+
+
+resource "azurerm_network_security_group" "vmsspatch" {
+  name                = "${var.prefix}-NSG"
+  location            = azurerm_resource_group.vmsspatchrg.location
+  resource_group_name = azurerm_resource_group.vmsspatchrg.name
+    
+  security_rule {
+      name                       = "SSH"
+      priority                   = 1001
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+  }
+
+    security_rule {
+      name                       = "HTTP"
+      priority                   = 1002
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg1" {
+  subnet_id                 = azurerm_subnet.vmssbpatch.id
+  network_security_group_id = azurerm_network_security_group.vmsspatch.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg2" {
+  subnet_id                 = azurerm_subnet.vmssbpatch2.id
+  network_security_group_id = azurerm_network_security_group.vmsspatch.id
+}
 
 #scale set built from Packer image
 resource "azurerm_virtual_machine_scale_set" "vmss" {
@@ -185,6 +248,72 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
       name                                   = "IPConfiguration"
       subnet_id                              = azurerm_subnet.vmssbpatch.id
       load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
+      primary = true
+    }
+  }
+  
+}
+
+
+
+
+resource "azurerm_virtual_machine_scale_set" "vmss2" {
+  name                = "${lower(var.prefix)}-vmss02"
+  location            = azurerm_resource_group.vmsspatchrg.location
+  resource_group_name = azurerm_resource_group.vmsspatchrg.name
+  upgrade_policy_mode = "Manual"
+
+  sku {
+    name     = "Standard_DS1_v2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
+    id=data.azurerm_shared_image_version.second.id
+    #publisher = "Canonical"
+    #offer     = "UbuntuServer"
+    #sku       = "18.04-LTS"
+    #version   = "latest"
+  }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_data_disk {
+    lun          = 0
+    caching        = "ReadWrite"
+    create_option  = "Empty"
+    disk_size_gb   = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "vmlab"
+    admin_username       = local.AdminUserName
+    admin_password       = random_string.pass1.result
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+
+#    ssh_keys {
+#      path     = "/home/azureuser/.ssh/authorized_keys"
+#      key_data = file("ssh_key/id_rsa.pub")
+#    }
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile2"
+    primary = true
+
+    ip_configuration {
+      name                                   = "IPConfiguration2"
+      subnet_id                              = azurerm_subnet.vmssbpatch2.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool2.id]
       primary = true
     }
   }
