ce0d0553dec6e2f50cecc0aa7a76f990646444d5
Ruslan Gatiyatullin <Ruslan.Gatiyatullin@icl-services.com>
2020-06-01 18:54:14 +0300

    ss02 deployment

--- VMSSPatching/terraform/st02.2/deploy_ss02.sh
+++ VMSSPatching/terraform/st02.2/deploy_ss02.sh
@@ -14,7 +14,7 @@ cd "$CLIENT_BUILD_DIR"
 
 #downloading manifests
 echo "Downloading the terraform manifest"
-wget -q https://raw.githubusercontent.com/sarcalier/azuretedeploy/dev/VMSSPatching/terraform/st01/prep_groups.tf -O prep_groups.tf
+wget -q https://raw.githubusercontent.com/sarcalier/azuretedeploy/dev/VMSSPatching/terraform/st02.2/main.tf -O main.tf
 
 
 #playing the manifest
