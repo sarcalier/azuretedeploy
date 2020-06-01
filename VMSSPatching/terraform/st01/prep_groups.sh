8366ef20e782bac489166f2db0cb226c2c098a06
Ruslan Gatiyatullin <Ruslan.Gatiyatullin@icl-services.com>
2020-06-01 11:21:30 +0300

    dum fix

--- VMSSPatching/terraform/st01/prep_groups.sh
+++ VMSSPatching/terraform/st01/prep_groups.sh
@@ -14,7 +14,7 @@ cd "$CLIENT_BUILD_DIR"
 
 #downloading manifests
 echo "Downloading the terraform manifest"
-wget -q https://raw.githubusercontent.com/sarcalier/azuretedeploy/dev/VMSSPatching/terraform/st01/prep_groups.tf -O prep_groups.t
+wget -q https://raw.githubusercontent.com/sarcalier/azuretedeploy/dev/VMSSPatching/terraform/st01/prep_groups.tf -O prep_groups.tf
 
 
 #playing the manifest
