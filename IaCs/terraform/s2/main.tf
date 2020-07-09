02447ab3c76edd3b35aaf588b6b04550d2a2a96f
Ruslan Gatiyatullin <Ruslan.Gatiyatullin@icl-services.com>
2020-07-09 11:45:00 +0300

    1st vm in AV zone 1

---
+++ IaCs/terraform/s2/main.tf
@@ -0,0 +1,234 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you are using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
+variable "prefix" {
+  description = "Type in the prefix to name resources in this deployment "
+  default = "RuslanG-RG"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-IaCs-tf-s2"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vpc01"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+
+
+resource "azurerm_subnet" "sub01" {
+  name                 = "subnet01"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes       = ["10.10.4.0/24"]
+}
+
+resource "azurerm_subnet" "sub02" {
+  name                 = "subnet02"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes       = ["10.10.5.0/24"]
+}
+
+
+
+resource "azurerm_public_ip" "main" {
+  name                = "pubpip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_lb" "main" {
+  name                = "vmss-lb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+
+}
+
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "BackEndAddressPool01"
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool2" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "BackEndAddressPool02"
+}
+
+resource "azurerm_lb_probe" "main" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "http-running-probe"
+  port                = 80
+}
+
+resource "azurerm_lb_rule" "lbnatrulehttp" {
+  resource_group_name            = azurerm_resource_group.main.name
+  loadbalancer_id                = azurerm_lb.main.id
+  name                           = "http"
+  protocol                       = "Tcp"
+  frontend_port                  = "80"
+  backend_port                   = "80"
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = azurerm_lb_probe.main.id
+}
+
+
+resource "azurerm_lb_rule" "lbnatrulessh" {
+  resource_group_name            = azurerm_resource_group.main.name
+  loadbalancer_id                = azurerm_lb.main.id
+  name                           = "ssh"
+  protocol                       = "Tcp"
+  frontend_port                  = "22"
+  backend_port                   = "22"
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+  #probe_id                       = azurerm_lb_probe.vmsspatch.id
+}
+
+
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-NSG"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+    
+  security_rule {
+      name                       = "SSH"
+      priority                   = 1001
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+  }
+
+    security_rule {
+      name                       = "HTTP"
+      priority                   = 1002
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "80"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg1" {
+  subnet_id                 = azurerm_subnet.sub01.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg2" {
+  subnet_id                 = azurerm_subnet.sub02.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.sub01.id
+    private_ip_address_allocation = "Dynamic"
+#    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+
+resource "random_string" "pass1" {
+  length  = 16
+  upper   = true
+  lower   = true
+  number  = true
+  special = true
+}
+
+locals {
+  AdminUserName = "tstadmin"
+}
+
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm01"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+  zones                 = [1]
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "vm01"
+    admin_username = local.AdminUserName
+    admin_password = random_string.pass1.result
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
+
+
+resource "azurerm_virtual_machine_extension" "main" {
+  name                 = "nginx"
+  virtual_machine_id   = azurerm_virtual_machine.main.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "apt-get update && apt-get install -y nginx "
+    }
+  SETTINGS
+  depends_on = [azurerm_virtual_machine.main]
+}
