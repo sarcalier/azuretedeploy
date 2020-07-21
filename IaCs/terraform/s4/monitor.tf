07f64b3d71293dc391559848ea41392f11e264f6
Ruslan Gatiyatullin <Ruslan.Gatiyatullin@icl-services.com>
2020-07-21 12:50:02 +0300

    LA unique fix

--- IaCs/terraform/s4/monitor.tf
+++ IaCs/terraform/s4/monitor.tf
@@ -1,5 +1,5 @@
 resource "azurerm_log_analytics_workspace" "main" {
-  name                = "logs-01"
+  name                = "logs-01-${random_string.hash.result}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   sku                 = "PerGB2018"
