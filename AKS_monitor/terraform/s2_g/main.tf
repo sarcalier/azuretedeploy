913615f5a10467eed54630e0f428d6c54bfd9d05
Ruslan Gatiyatullin <Ruslan.Gatiyatullin@icl-services.com>
2020-11-11 23:19:26 +0300

    another k8s cluster

---
+++ AKS_monitor/terraform/s2_g/main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you are using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
+resource "random_id" "log_analytics_workspace_name_suffix" {
+    byte_length = 8
+}
+
+
+data "azurerm_resource_group" "main" {
+  name     = "POC2-IaC-AKSmon"
+}
---
+++ AKS_monitor/terraform/s2_g/variables.tf
@@ -0,0 +1,44 @@
+variable "client_id" {
+    description = "Service Principal ID"
+}
+variable "client_secret" {
+    description = "Service Principal password"
+}
+
+variable "agent_count" {
+    default = 2
+}
+
+#variable "ssh_public_key" {
+#    default = "~/.ssh/id_rsa.pub"
+#}
+
+variable "dns_prefix" {
+    default = "k8spoc2"
+}
+
+variable cluster_name {
+    default = "k8spoc2"
+}
+
+#variable resource_group_name {
+#    default = "azure-k8stest"
+#}
+
+variable location {
+    default = "West Europe"
+}
+
+variable log_analytics_workspace_name {
+    default = "LogAnalyticsWorkspaceName"
+}
+
+# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
+variable log_analytics_workspace_location {
+    default = "westeurope"
+}
+
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
+}
